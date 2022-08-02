906852c3dbe7dd8183267d69b4b846d93601917b
LeanMorande <ldmorande@hotmail.com>
2022-08-02 00:09:57 -0300

    git push

--- Js/app.js
+++ Js/app.js
@@ -1,24 +1,20 @@
+const forma = document.querySelector("#formulario");
+forma.addEventListener("submit", Ingresar);
 
-      
-
-      const forma = document.querySelector("#formulario");
-      console.log(forma);
-
-      forma.addEventListener("submit", mostrarConsola);
-
-      function mostrarConsola(e) {
-        e.preventDefault();
-        const usuario = document.querySelector("#usuario").value;
-        const pass = document.querySelector("#pass").value;
-        console.log(usuario, pass);
-      }
-      window.addEventListener("scroll", function () {
-        let navArea = document.getElementById("navArea");
-
-        if (window.pageYOffset > 0) {
-          navArea.classList.add("is-sticky");
-        } else {
-          navArea.classList.remove("is-sticky");
-        }
-      });
-    
+function Ingresar() {
+  const usuario = document.querySelector("#usuario").value;
+  const pass = document.querySelector("#pass").value;
+  if (usuario == "rodridega@gmail.com" && pass == "123456") {
+setTimeout(() => {  location.href = "portada.html";
+});
+} else {
+    alert("fuera bicho!");
+    Index();
+  }
+}
+function Index() {
+  location.href = "index.html";
+}
+function Portada() {
+  location.href = "portada.html";
+}
